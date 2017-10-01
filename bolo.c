80ba91518344989f1991cf6e2971d189a19772b4
James Hunt <james@niftylogic.com>
2017-10-01 14:57:15 +0000

    New `bolo idxinfo` for dumping btrees

--- bolo.c
+++ bolo.c
@@ -5,6 +5,7 @@
 
     bolo [-v|--version]
 
+    bolo idxinfo FILE
     bolo slabinfo FILE
     bolo version
     bolo stdin
@@ -12,6 +13,41 @@
     (other commands and options added as necessary)
 
  */
+
+static int
+do_idxinfo(int argc, char **argv)
+{
+	struct btree *idx;
+	const char *path;
+	int i, fd;
+
+	if (argc < 3) {
+		fprintf(stderr, "USAGE: bolo idxinfo FILE\n");
+		return 1;
+	}
+
+	for (i = 2; i < argc; i++) {
+		path = argv[i];
+		fd = open(path, O_RDONLY);
+		if (fd < 0) {
+			fprintf(stderr, "failed to open %s: %s\n", path, error(errno));
+			continue;
+		}
+
+		idx = btree_read(fd);
+		if (!idx) {
+			fprintf(stderr, "failed to read %s: %s\n", path, error(errno));
+			continue;
+		}
+
+		fprintf(stdout, "IDX %s\n", path);
+		btree_print(idx);
+
+		btree_close(idx);
+		close(fd);
+	}
+	return 0;
+}
 
 static int
 do_slabinfo(int argc, char **argv)
@@ -175,6 +211,9 @@ int main(int argc, char **argv)
 
 	if (strcmp(command, "stdin") == 0)
 		return do_stdin(argc, argv);
+
+	if (strcmp(command, "idxinfo") == 0)
+		return do_idxinfo(argc, argv);
 
 	if (strcmp(command, "slabinfo") == 0)
 		return do_slabinfo(argc, argv);
